2d5cce6326684fa965149dc21174389c34a9e19f
Michael Woodyard <mbwoodyard@hotmail.com>
2021-12-27 17:20:15 -0500

    first commit

---
+++ game.py
@@ -0,0 +1,112 @@
+import pygame
+import random
+import math
+pygame.init()
+
+class DrawInformation:
+	# Global Values
+	BLACK = 0, 0, 0
+	WHITE = 255, 255, 255
+	GREEN = 0, 255, 0
+	RED   = 255, 0, 0
+	# GREY  = 128, 128, 128
+	BACKGROUND_COLOR = WHITE
+
+	GRADIENTS = [
+		(128, 128, 128), # Grey
+		(160, 160, 160), # Darker Grey
+		(192, 192, 192)  # Dark Grey
+	]
+
+	SIDE_PAD = 100 # 100 Pixels, 50 pixels on left, 50 on right.
+	TOP_PAD = 150
+
+	def __init__(self, width, height, lst):
+		self.width  = width
+		self.height = height
+
+		# Creates a window for the game with width and height.
+		self.window = pygame.display.set_mode((width, height))
+		pygame.display.set_caption("Sorting Algorithm Visualization")
+		self.set_list(lst)
+
+	def set_list(self, lst):
+		self.lst       = lst
+		self.min_val   = min(lst)
+		self.max_val = max(lst)
+
+		self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+		self.block_height = math.floor((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+		self.start_x = self.SIDE_PAD // 2
+
+# Draws the entire screen.
+def draw(draw_info):
+	draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+	draw_list(draw_info)
+	pygame.display.update()
+
+# Draws the values from the list onto the screen.
+def draw_list(draw_info):
+	lst = draw_info.lst
+
+	for i, val in enumerate(lst): # Index and value in list.
+		x = draw_info.start_x + i * draw_info.block_width
+		y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+
+		color = draw_info.GRADIENTS[i % 3] # 0, 1, or 2
+
+		pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.block_height))
+
+# Generates a list of random numbers.
+def generate_starting_list(n, min_val, max_val):
+	lst = []
+
+	for _ in range(n):
+		val = random.randint(min_val, max_val)
+		lst.append(val)
+
+	return lst
+
+def main():
+	run     = True
+	clock   = pygame.time.Clock()
+	n       = 50
+	min_val = 0
+	max_val = 100
+
+	lst        = generate_starting_list(n, min_val, max_val)
+	draw_info  = DrawInformation(800, 600, lst)
+	sorting    = False
+	ascending  = True
+	descending = False
+
+	while run:
+		clock.tick(60)
+
+		draw(draw_info)
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: # Closes the program when X is clicked.
+				run = False
+			
+			if event.type != pygame.KEYDOWN:
+				continue
+
+			if event.key == pygame.K_r: # R key.
+				lst = generate_starting_list(n, min_val, max_val)
+				draw_info.set_list(lst)
+				sorting = False
+
+			elif event.key == pygame.K_SPACE and sorting == False: # R key.
+				sorting = True
+
+			elif event.key == pygame.K_a and not sorting: # A key.
+				ascending = True
+
+			elif event.key == pygame.K_d and not sorting: # D key.
+				ascending = False
+
+	pygame.quit()
+
+if __name__ == "__main__":
+	main()
